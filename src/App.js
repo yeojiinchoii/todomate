46edd67f40cc1c35b488c7728fc3a8a3f6ea8952
Yeojin Choi <yeojiinchoii@gmail.com>
2023-04-18 22:04:37 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -110,5 +110,4 @@ function App() {
   );
 }
 
-
 export default App;
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -12,11 +12,11 @@ const UpdateForm = ({ updateData, changeHolder, updateTask, cancelUpdate }) => {
           <div className="col-auto">
             <button
               onClick={updateTask}
-              className="btn btn-lg mr-20"
+              className="btn btn-lg btn-update"
             >Update</button>
             <button
               onClick={cancelUpdate}
-              className="btn btn-lg"
+              className="btn btn-lg btn-cancel"
             >Cancel</button>
           </div>
         </div>
